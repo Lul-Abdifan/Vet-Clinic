efb847bca607b7f9e8dedfbb3f3582d2d44ffd38
Lul-Abdifan <tasanbannu@gmail.com>
2023-06-08 19:09:20 -0400

    AddUpdate query multiple table datas

--- data.sql
+++ data.sql
@@ -15,3 +15,58 @@ VALUES ('Pikachu', '2021-01-07', 15.04, FALSE, 1);
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
 VALUES ('Devimon', '2017-05-12', 11, TRUE, 5);
 
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES
+    ('Charmander', '2020-02-08', 0, FALSE, -11),
+    ('Plantmon', '2021-11-15', 2, TRUE, -5.7),
+    ('Squirtle', '1993-04-02', 3, FALSE, -12.13),
+    ('Angemon', '2005-06-12', 1, TRUE, -45),
+    ('Boarmon', '2005-06-07', 7, TRUE, 20.4),
+    ('Blossom', '1998-10-13', 3, TRUE, 17),
+    ('Ditto', '2022-05-14', 4, TRUE, 22);
+
+-- query multiple tables branch
+
+INSERT INTO owners (name, age)
+VALUES 
+    ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
+
+INSERT INTO species(name) 
+ VALUES
+    ('Pokemon'),
+    ('Digimon');
+UPDATE animals
+SET species_id = 2
+where name like '%mon';
+
+UPDATE animals
+SET species_id = 1
+WHERE name NOT LIKE '%mon';
+
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+WHERE name = 'Agumon';
+
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+WHERE name = 'Gabumon' OR name = 'Pikachu';
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob')
+WHERE name IN('Devimon','Plantmon');
+
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+WHERE name IN('Squirtle','Charmander','Blossom');
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+WHERE name IN('Boarmon','Angemon');
