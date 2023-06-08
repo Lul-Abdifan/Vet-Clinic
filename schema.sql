761e8fdfedb534a9ae82ee9bef15d464e163ba1a
Lul-Abdifan <tasanbannu@gmail.com>
2023-06-08 19:07:57 -0400

    Add schema for the query multiple

--- schema.sql
+++ schema.sql
@@ -7,3 +7,37 @@ CREATE TABLE animals (
     neutered BOOLEAN,
     weight_kg DECIMAL(10, 2)
 );
+
+-- query multiple tables branch
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(75),
+  age INT
+);
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50)
+);
+
+ALTER TABLE animals
+DROP COLUMN species ;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species(id)
+ON DELETE CASCADE;
+
+
+ALTER TABLE animals
+    ADD COLUMN species_id INTEGER,
+    ADD CONSTRAINT fk_species
+    FOREIGN KEY (species_id)
+    REFERENCES species(id)
+    ON DELETE CASCADE;
+
+
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT REFERENCES owners(id) ON DELETE CASCADE;
